fc249f43227dc4e5be70dd13d8d699c46c88aceb
bluepenguin14 <pweill70@yahoo.com>
2026-06-22 10:27:35 -0400

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,13 +1,21 @@
 async function enterRaffle() {
-  const name =
-    document.getElementById("name").value;
+const name =
+  document.getElementById("name").value;
+
+const tickets =
+  parseInt(
+    document.getElementById("tickets").value
+  ) || 1;
 
   const res = await fetch("/api/enter", {
     method: "POST",
     headers: {
       "Content-Type":"application/json"
     },
-    body: JSON.stringify({ name })
+    body: JSON.stringify({
+  name,
+  tickets
+})
   });
 
   const data = await res.json();
